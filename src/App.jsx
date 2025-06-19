acea774c56db065a4db55e462046bf353bcaa46a
eddydpan <eddydotpan@gmail.com>
2025-06-19 18:29:49 -0400

    Import fixes--working state

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import AboutSection from './components/AboutSection';
 import Portfolio from './components/Portfolio';
-import Subpage from './components/Subpage';
-import BettaFishChessPlayerSubpage from './components/subpages/BettaFishChessPlayerSubpage';
-import IronManSubpage from './components/subpages/IronManSubpage';
+import BettaFishChessPlayerSubpage from './components/subpages/projects/BettaFishChessPlayerSubpage';
+import IronManSubpage from './components/subpages/projects/IronManSubpage';
 
 export default function App() {
   return (
@@ -23,7 +22,6 @@ export default function App() {
           />
           <Route path="/about" element={<AboutSection />} />
           <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/portfolio/:slug" element={<Subpage />} />
           <Route path="/portfolio/bettafish-chess-player" element={<BettaFishChessPlayerSubpage />} />
           <Route path="/portfolio/iron-man" element={<IronManSubpage />} />
         </Routes>

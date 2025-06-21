bf7d903958564149ce5cf948aafc26bab186600b
eddydpan <eddydotpan@gmail.com>
2025-06-21 15:38:46 -0400

    Write Iron Man subpage

---
+++ my-portfolio/tailwind.config.cjs
@@ -0,0 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'], // Adjust paths to match your project structure
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
